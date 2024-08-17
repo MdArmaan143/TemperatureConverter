d77fbf09e21983400886c6b00e9cdc19c0f1df5c
Md Armaan <armaanshaikhh786@gmail.com>
2024-08-18 00:06:29 +0530

    add transition and transform

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var cel = document.getElementById("cel");
-        var fah = document.getElementById("fah");
+var fah = document.getElementById("fah");
 
-        cel.addEventListener('input', function(){
-            let c = parseFloat(this.value);
-            let f = (c * 9 / 5) + 32;
-            fah.value = f.toFixed(2);
-        });
+cel.addEventListener('input', function(){
+    let c = parseFloat(this.value);
+    let f = (c * 9 / 5) + 32;
+    fah.value = f.toFixed(2);
+});
 
-        fah.addEventListener('input', function(){
-            let f = parseFloat(this.value);
-            let c = (f - 32) * 5 / 9;
-            cel.value = c.toFixed(2);
-        });
+fah.addEventListener('input', function(){
+    let f = parseFloat(this.value);
+    let c = (f - 32) * 5 / 9;
+    cel.value = c.toFixed(2);
+});
 
-        function getRandomColor() {
-            var letters = '0123456789ABCDEF';
-            var color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-        function changeBackgroundColor() {
-            document.body.style.backgroundColor = getRandomColor();
-        }
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
 
-        setInterval(changeBackgroundColor, 3000); // Change color every 3 seconds
+setInterval(changeBackgroundColor, 3000); // Change color every 3 seconds
